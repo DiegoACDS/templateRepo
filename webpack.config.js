3382fc9d2b821f820ab07dbd7687822666de4251
DiegoACDS <dede2040a@gmail.com>
2025-05-08 22:30:24 -0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const htmlwebpackplugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        clean: true,
+    },
+    plugins: [
+        new htmlwebpackplugin({
+            title: 'Minha Todo List',
+            template: 'src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        fallback: {
+            "fs": false,
+            "child_process": false,
+            "worker_threads": false,
+        }
+    }
+    
+};
